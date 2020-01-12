f8a5bda7f688557da84931732a938d12c1de5d60
JudahZammit <43350543+JudahZammit@users.noreply.github.com>
2020-01-12 13:07:40 -0600

    Added the ability to read multiple puzzles from one file
    Added the ability to read multiple puzzles from one file

--- backtracking.py
+++ backtracking.py
@@ -1,8 +1,23 @@
 import rule
-def backtracking(state): #state is the initial state immediately after read from stdin
+from read import printPuzzle,read,readFile
+
+TEST_FILE = 'test_puzzles.txt'
+VERBOSE = False
+
+#the main loop so that it will always ask an input then solve the puzzle
+def mainLoop():
+    for puzzle in readFile(TEST_FILE):
+        state=puzzle['puzzle']
+        assignment=backtracking(state)
+        temp=assignAll(assignment,state) #the final solution as a 2d list
+        print('the answer is:')
+        printPuzzle(temp)
+
+
+def backtracking(state): #state is the initial state immediately after reading from stdin
     U=rule.getUnassigned(state) #a list of all unassigned variables
     A = {} #a dictionary that maps: unassigned coordinate-->None
-    return backtrackingHelper(A,U,state)
+    return backtrackingHelper(A,U.copy(),state)
 
 
 
@@ -11,20 +26,25 @@ def backtracking(state): #state is the initial state immediately after read from
 #it returns a new assignment to solve the puzzle with initial state "state"
 def backtrackingHelper(A,U,state):
     current = assignAll(A, state)
-    if len(A)==len(state)*len(state[0])-rule.getNumbers(state) and rule.solutionCheck(current):#all unassigned cell have been assigned
+    # Only prints the temporary states if VERBOSE is true
+    if VERBOSE:
+        printPuzzle(current)
+    if rule.solutionCheck(current):#all unassigned cell have been assigned
         return A                                                                             #and the number constraint is satisfied
-    (i,j)=U.pop(0)
-    for option in ['_','b']:    #one coordinate has 2 possible assignment
-        if option=='b' and ((i,j) not in rule.goodCells(current)):
-            break
-        B=A.copy()
-        B[(i,j)]=option         #add one more assignment to list A
-        temp=assignAll(B,state) #apply assignments to original state
-        if option=='b' and not rule.numberConstrain(temp):
-            break
-        result=backtrackingHelper(B,U.copy(),state) #recursive call with new assignments B
-        if result is not None:                  # WE HAVE not solution in this case
-            return result
+    elif len(U)>0:
+        (i,j)=U.pop(0)
+        for option in ['b','_']:    #one coordinate has 2 possible assignment
+            B = A.copy()
+            B[(i, j)] = option
+            if ((i,j) not in rule.goodCells(current)) and option=='b':
+                continue
+            temp=assignAll(B,state)
+            if not rule.numberConstrain(temp):
+                continue
+            result=backtrackingHelper(B,U.copy(),state) #recursive call with new assignments B
+            if result is not None:                  # WE HAVE no solution in this case
+                return result
+
     return None
 
 
@@ -35,13 +55,13 @@ def backtrackingHelper(A,U,state):
 
 #this method receives an initial state and apply assignment A to it then return its copy
 def assignAll(A,state):
-    result=state.clone()
+    result=state.copy()
     for (i,j),option in A.items():
         result[i][j]=option
     return result
 
 
-#
+
 
 
 
--- read.py
+++ read.py
@@ -1,14 +1,19 @@
 
 import sys
-row=0  #the number of rows
-col=0  #the number of cols
-puzzle=[] #a 2d-array
-data={'row':0,'col':0,'puzzle':puzzle} #this is the dictionary, row&col are integer and puzzle is a 2-d array
 
-#use the format provided in the assignment(i.e. the first 2 lines must specify the start and the dimension)
+#use the format provided in the assignment(i.e. the first 2 lines must specify the start and the dimension
+#the last line must specify the end )
 def read():         #this method reads from stdin and returns a dictionary containing a 2d-array
     print("plz enter your puzzle")
     linesArr = sys.stdin.read().splitlines()
+    return readLineArr(linesArr)
+
+def readLineArr(linesArr):
+    row=0  #the number of rows
+    col=0  #the number of cols
+    puzzle=[] #a 2d-array
+    data={'row':0,'col':0,'puzzle':puzzle} #this is the dictionary, row&col are integer and puzzle is a 2-d array
+    
     temp=[int(s) for s in linesArr[1].split() if s.isdigit()]   #temp stores number of rows and cols
     row, col=temp[0], temp[1]
     puzzle1=[[0 for x in range(col)]for y in range(row)]    #first initialize this 2d-array with 0's
@@ -19,5 +24,36 @@ def read():         #this method reads from stdin and returns a dictionary conta
             else:puzzle1[i][j]=linesArr[i][j]
     data['row'], data['col'], data['puzzle']=row,col,puzzle1    #assign new values to the dictionary
     return data
+    
+    
+def readFile(filename):
+    allLines = open(filename, "r").read().splitlines()
+    
+    
+    size = len(allLines) 
+    idx_list = [idx + 1 for idx, val in
+                enumerate(allLines) if val == '#End'] 
+  
+
+    puzzleList = [allLines[i: j] for i, j in
+            zip([0] + idx_list, idx_list + 
+            ([size] if idx_list[-1] != size else []))] 
+    
+    dataList = []
+    for linesArr in puzzleList:
+        dataList.append(readLineArr(linesArr))
+    
+    return dataList
+    
+
+
+#print out an 2d array(the snapshot of the puzzle)
+def printPuzzle(arr):
+    print('******************************************')
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            print(arr[i][j], end="")
+        print()
+    print('******************************************')
 
 
